3330eab71d2ce9b31a36f269a4ea46e1abaacac7
keroken <kenichi.shinagawa@gmail.com>
2021-05-15 15:03:55 +0900

    ref example

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import ReactDOM from 'react-dom';
 import GuestList from './state/GuestList';
 import UserSearch from './state/UserSearch';
 import EventComponent from './events/EventComponent';
+import UserSearchRef from './refs/UserSearch';
 
 const App = () => {
   return <div>
     <GuestList />
     <UserSearch />
+    <UserSearchRef />
     <EventComponent />
   </div>
 };
